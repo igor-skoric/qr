4604677f512b68d484544ad26e46c5935fe750d0
Igor <iskoric95@gmail.com>
2025-04-01 11:45:22 +0200

    ws

--- qr/asgi.py
+++ qr/asgi.py
@@ -1,10 +1,13 @@
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qr.settings')
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from qr.routing import websocket_urlpatterns  # Dodaj ovaj import
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qr.settings')
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
