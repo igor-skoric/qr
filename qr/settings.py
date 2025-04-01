ad9fa89761674418ca897967669ca592a35bd5b1
Igor <iskoric95@gmail.com>
2025-04-01 12:07:35 +0200

    allowed hosts

--- qr/settings.py
+++ qr/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4)ga@&r9eoxdi9ui_hw0qw0bqqc2u6+hf6iq66-g@bnhb%$ai5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['testqrcode.rs.itbranch.rs', '127.0.0.1', 'qr-codes-1d661a844374.herokuapp.com']
 
