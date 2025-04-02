3743695bcb8d618b1e6288e437e3e98372c1749b
Igor <iskoric95@gmail.com>
2025-04-02 18:33:21 +0200

    claudinary

--- website/signals.py
+++ website/signals.py
@@ -6,21 +6,21 @@ from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 
 
-@receiver(post_delete, sender=Image)
-def delete_image_files(sender, instance, **kwargs):
-    """ Briše slike iz fajl sistema kada se model obriše iz baze. """
-
-    # Briše glavnu sliku
-    if instance.image:
-        image_path = instance.image.path
-        if os.path.isfile(image_path):
-            os.remove(image_path)
-
-    # Briše QR kod ako postoji
-    if instance.qr_code:
-        qr_code_path = instance.qr_code.path
-        if os.path.isfile(qr_code_path):
-            os.remove(qr_code_path)
+# @receiver(post_delete, sender=Image)
+# def delete_image_files(sender, instance, **kwargs):
+#     """ Briše slike iz fajl sistema kada se model obriše iz baze. """
+#
+#     # Briše glavnu sliku
+#     if instance.image:
+#         image_path = instance.image.path
+#         if os.path.isfile(image_path):
+#             os.remove(image_path)
+#
+#     # Briše QR kod ako postoji
+#     if instance.qr_code:
+#         qr_code_path = instance.qr_code.path
+#         if os.path.isfile(qr_code_path):
+#             os.remove(qr_code_path)
 
 
 @receiver(post_save, sender=Image)
